5731c1261d1b295ce4033013ea65b690b637fd6e
Vladyslav <chuck_99_ua@ukr.net>
2023-08-01 23:22:08 +0300

    All without brut-force

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.file.Files;
@@ -14,63 +13,59 @@ public class Main {
     public static void main(String[] args) {
         String exit = "exit";
         Scanner scan = new Scanner(System.in);
-        boolean cycle = true;
+        boolean cycle;
         byte operation = 0;  //1 - encrypt, -1 - decrypt
         int filesProcessed = 0;
-        int key;
+        byte key = 0;
         boolean cycleMenu = true;
-
 
         while (cycleMenu){
 
+            cycle = true;
             while (cycle) {
                 System.out.println("Оберіть дію: 1 - Шифруємо файл, -1 - Дешифруємо (тільки 1 або -1)");
                 if (scan.hasNextByte()) {
                     operation = scan.nextByte();
+                    scan.nextLine();
                     if (operation == 1 || operation == -1) {
                         cycle = false;
                     }
                 } else {
-                    scan.next();
+                    scan.nextLine();
                 }
             }
             cycle = true;
             while (cycle) {
                 System.out.println("Укажіть шлях до файлу:");
-                if (scan.hasNextLine()) {
-                    sourceFilePath = scan.nextLine();
-
-                    Path pathFile = Path.of(sourceFilePath);
-                    if (!Files.isRegularFile(pathFile)) {
-                        System.out.println("Файл не існує, повтори введення");
-                    } else {
+                sourceFilePath = scan.nextLine();
+                Path pathFile = Path.of(sourceFilePath);
+                if (!Files.isRegularFile(pathFile)) {
+                    System.out.println("Файл не існує, повтори введення");
+                } else {
                         cycle = false;
 
                         String fileName = pathFile.getFileName().toString();
                         String fileNameWithoutExtension = fileName.substring(0, (fileName.lastIndexOf('.')==-1) ? fileName.length() : fileName.lastIndexOf('.'));
-                        String fileNameExtension = fileName.substring((fileName.lastIndexOf('.')==-1) ? "" :  fileName.lastIndexOf('.'));
+                        String fileNameExtension = fileName.substring((fileName.lastIndexOf('.')==-1) ? 0 :  fileName.lastIndexOf('.'));
 
                         destinationFilePath = pathFile.getParent().toString()+fileNameWithoutExtension+"_"+filesProcessed+fileNameExtension;
-                    }
-                } else {
-                    scan.next();
                 }
             }
             cycle = true;
             while (cycle) {
-                System.out.println("Укажіть ключ шифрування\/дешифрування(0 то буде виконо метобом брут-форс):");
+                System.out.println("Укажіть ключ шифрування/дешифрування(0 то буде виконо метобом брут-форс):");
                 if (scan.hasNextByte()) {
                     key = scan.nextByte();
+                    scan.nextLine();
                     cycle = false;
                 } else {
-                    scan.next();
+                    scan.nextLine();
                 }
             }
             filesProcessed ++;
 
-
-            try (FileReader fileRead = new FileReader("F:\\fileReader.txt");
-                 FileWriter filewriter = new FileWriter("F:\\fileReader-2.txt");
+            try (FileReader fileRead = new FileReader(sourceFilePath);
+                 FileWriter filewriter = new FileWriter(destinationFilePath);
                  BufferedReader reader = new BufferedReader(fileRead) ) {
 
                 while (reader.ready()) {
@@ -80,11 +75,16 @@ public class Main {
             } catch (Exception e) {
                 System.out.println("Something went wrong : " + e);
             }
-
+            System.out.println("Для завершення роботи введіть \"exit\" або для повторення операцій");
+            String str = scan.nextLine();
+            if (str.toLowerCase().equals(exit)) {
+                cycleMenu = false;
+                scan.close();
+            }
         }
     }
 
-    public static String Encrypt_decrypt(String string, int key, byte operation){
+    public static String Encrypt_decrypt(String string, byte key, byte operation){
 
         StringBuilder strBuild = new StringBuilder();
         int leng = alphabetUkr.length();
